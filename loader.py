421ca642b55dc1a2ffdd57b2610915f005135061
Arturo Alcala <alcala28043@gmail.com>
2026-02-15 17:15:53 +0100

    fix: fix relative imports at loader

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@
 Expose `register_nodes()` which returns a list of node classes that a
 host application (or test harness) can import and register.
 """
-from nodes import KPUExampleNode
+from .nodes import KPUExampleNode
 
 
 def register_nodes():
